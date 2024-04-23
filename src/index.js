d4d2b7f0ebcb52f2ce19e95de536b76b7b67f64b
MatiasRamos <matias@e-cruce.com>
2024-04-23 08:48:41 -0300

    add pagination on endpoint

--- src/index.js
+++ src/index.js
@@ -10,16 +10,34 @@ const products = require( '../data/products.json' );
 app.use( cors() )
 // Endpoint para buscar items
 app.get( '/api/items', ( req, res ) => {
-    const query = req.query[ 'q' ].toLowerCase();
-    const filteredProducts = products.products.filter( product =>
-        product.title.toLowerCase().includes( query )
+    const { q, skip = 0, limit = 30 } = req.query;
+
+    // Filtrar productos basados en la consulta de búsqueda
+    let filteredProducts = products.products.filter( product =>
+        product.title.toLowerCase().includes( q.toLowerCase() )
     );
-    res.json( filteredProducts );
+
+    // Calcular el total de productos encontrados antes de aplicar paginación
+    const total = filteredProducts.length;
+
+    // Aplicar paginación a los productos filtrados
+    filteredProducts = filteredProducts.slice( parseInt( skip ), parseInt( skip ) + parseInt( limit ) );
+
+    // Crear el objeto de respuesta
+    const response = {
+        products: filteredProducts,
+        total: total,
+        skip: parseInt( skip ),
+        limit: parseInt( limit )
+    };
+
+    res.json( response );
 } );
 
 // Endpoint para obtener detalles de un producto específico
 app.get( '/api/items/:id', ( req, res ) => {
     const { id } = req.params;
+    console.log( "id", id )
     const product = products.products.find( product => product.id == id );
     if ( product ) {
         res.json( product );
